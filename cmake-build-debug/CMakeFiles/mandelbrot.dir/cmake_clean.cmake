bc820703d6260e045c4b2914d317f5d3582e6cfc
D Nishiyama <40795078+ProfFunami@users.noreply.github.com>
2019-01-31 14:09:49 +0900

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/mandelbrot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mandelbrot.dir/mandelbrot.c.o"
+  "mandelbrot.pdb"
+  "mandelbrot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mandelbrot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
